852d26751ca90ce4e54585c55b0463a697c52f7a
ewrayjohnson <wray.johnson@gmail.com>
2025-03-03 10:14:17 -0500

    Code cleanup

--- index.ts
+++ index.ts
@@ -4,7 +4,7 @@ const _ = require('lodash');
 const jsonld = require('jsonld');
 const ldtr = require('ldtr');
 const { Parser, Writer } = require('n3');
-const { Graph, /* Rdfs, print,*/ namespaces, converters, utils } = require('@entryscape/rdfjson');
+const { Graph, namespaces, converters, utils } = require('@entryscape/rdfjson');
 const xml2js = require('xml2js');
 
 const XML_SCHEMA_URI = 'http://www.w3.org/2001/XMLSchema';
@@ -35,7 +35,6 @@ type Packages = {
 }
 
 (async () => {
-  // const filename = require('path').resolve(__dirname, '../.ciartifacts/config/defaultPrefixes.json');
   const defaultPrefixes = JSON.parse(fs.readFileSync(path.join(INPUT_DIR, 'config', 'defaultPrefixes.json'), 'utf8'));
   for (const [prefix, iri] of Object.entries(defaultPrefixes)) {
     namespaces.add(prefix, iri);
@@ -107,15 +106,11 @@ type Packages = {
           for (const [iri, prefix] of Object.entries(standalone.namespaces)) {
             namespaces.add(prefix, iri);
           }
-          // const defaultPrefix = defaultNs && standalone.namespaces[defaultNs];
-          const idPrefix = /*defaultPrefix ? `${defaultPrefix}:` : */`${defaultNs}`;
+          const idPrefix = `${defaultNs}`;
           const xsdPrefix = standalone.namespaces[XML_SCHEMA_URI];
           standalone.namespaces['http://www.w3.org/2001/XMLSchema#'] = xsdPrefix;
           if (xsdPrefix) {
             const elements = schema[`${xsdPrefix}:element`];
-            if (elements) {
-              // debugger;
-            }
             const attributes = schema[`${xsdPrefix}:attribute`];
             if (attributes) {
               for (const anAttribute of attributes) {
@@ -127,7 +122,6 @@ type Packages = {
                     if (attributeType) {
                       if (attributeName) {
                         const attributeId = `${idPrefix}${attributeName}`;
-                        // standalone.g.add(attributeId, 'rdf:Property', 'rdf:Property');
                         standalone.g.add(attributeId, RDF_TYPE, 'rdf:Property')
                         standalone.namespaces[OWL_URI] = 'owl';
                         standalone.namespaces[RDFS_URI] = 'rdfs';
@@ -149,8 +143,6 @@ type Packages = {
                           }
                         }
                       }
-                    } else {
-                      // debugger;
                     }
                   }
                 } else {
@@ -210,143 +202,94 @@ type Packages = {
                 const union = aSimpleType[`${xsdPrefix}:union`];
                 if (union) {
                   const types = union[0][`${xsdPrefix}:simpleType`];
-                  if (types /*&& typeof types[Symbol.iterator] === 'function'*/) {
+                  if (types) {
                     for (const aUnionSimpleType of types) {
                       handleRestrictions(aUnionSimpleType);
                     }
-                  } else {
-                    // console.log(types);
                   }
                 }
               }
             }
-          /*if (concepts.length > 0)*/ {
-              const typeSource = listSource || enumSource;
-              if (typeof typeSource === 'object' && typeSource['$']) {
-                const schemeName = typeSource['$']['name'];
-                if (schemeName) {
-                  schemeId = `${idPrefix}${schemeName}`;
-                  const annotation = typeSource[`${xsdPrefix}:annotation`];
-                  const documentation = annotation[0][`${xsdPrefix}:documentation`];
-                  const description = documentation && documentation[0];
-                  standalone.g.add(schemeId, RDF_TYPE, 'skos:ConceptScheme');
-                  if (description) {
-                    standalone.g.addL(schemeId, 'dc:title', removeWhitespace(description)/*, 'en-latn'*/);
-                  }
-                }
 
-                const allowedNotationsId = `${schemeId}Values`;
-                for (const aConcept of concepts) {
-                  standalone.g.add(schemeId, 'skos:hasTopConcept', aConcept.conceptId);
-                  standalone.namespaces[OWL_URI] = 'owl';
-                  standalone.g.addL(allowedNotationsId, 'owl:oneOf', aConcept.notation);
-                  standalone.g.add(aConcept.conceptId, RDF_TYPE, 'skos:Concept');
-                  const stmt = standalone.g.add(aConcept.conceptId, 'skos:inScheme', schemeId);
-                  // standalone.g.addL(aConcept.conceptId, 'skos:prefLabel', aConcept.notation, IC-EDH);
-                  standalone.g.addD(aConcept.conceptId, 'skos:notation', aConcept.notation/*, `${xsdPrefix}:string`*/);
-                  if (aConcept.prefLabel) {
-                    standalone.g.addL(aConcept.conceptId, 'skos:prefLabel', aConcept.prefLabel/*, 'en-latn'*/);
-                  }
+            const typeSource = listSource || enumSource;
+            if (typeof typeSource === 'object' && typeSource['$']) {
+              const schemeName = typeSource['$']['name'];
+              if (schemeName) {
+                schemeId = `${idPrefix}${schemeName}`;
+                const annotation = typeSource[`${xsdPrefix}:annotation`];
+                const documentation = annotation[0][`${xsdPrefix}:documentation`];
+                const description = documentation && documentation[0];
+                standalone.g.add(schemeId, RDF_TYPE, 'skos:ConceptScheme');
+                if (description) {
+                  standalone.g.addL(schemeId, 'dc:title', removeWhitespace(description));
                 }
-                standalone.namespaces[RDFS_URI] = 'rdfs';
-                standalone.g.add(allowedNotationsId, RDF_TYPE, 'rdfs:Datatype');
               }
-              // if (!standalone.namespaces[OWL_URI]) {
-              delete standalone.namespaces[XML_SCHEMA_URI];
-              // delete standalone.namespaces[RDF_URI];
-              // }
-              const extname = path.extname(inputFilepath);
-              const dirname = path.dirname(inputFilepath);
-              const basename = path.basename(inputFilepath, extname);
-              const relative = path.relative(schemasDir, dirname);
-              const outputBase = path.join(__dirname, '..', 'transformed');
 
-              convienence.g.addAll(standalone.g);
-              Object.assign(convienence.namespaces, standalone.namespaces);
-              await writeGraph(convienence, path.join(outputBase, 'convenience'));
+              const allowedNotationsId = `${schemeId}Values`;
+              for (const aConcept of concepts) {
+                standalone.g.add(schemeId, 'skos:hasTopConcept', aConcept.conceptId);
+                standalone.namespaces[OWL_URI] = 'owl';
+                standalone.g.addL(allowedNotationsId, 'owl:oneOf', aConcept.notation);
+                standalone.g.add(aConcept.conceptId, RDF_TYPE, 'skos:Concept');
+                const stmt = standalone.g.add(aConcept.conceptId, 'skos:inScheme', schemeId);
+                standalone.g.addD(aConcept.conceptId, 'skos:notation', aConcept.notation);
+                if (aConcept.prefLabel) {
+                  standalone.g.addL(aConcept.conceptId, 'skos:prefLabel', aConcept.prefLabel);
+                }
+              }
+              standalone.namespaces[RDFS_URI] = 'rdfs';
+              standalone.g.add(allowedNotationsId, RDF_TYPE, 'rdfs:Datatype');
+            }
+            delete standalone.namespaces[XML_SCHEMA_URI];
+            const extname = path.extname(inputFilepath);
+            const dirname = path.dirname(inputFilepath);
+            const basename = path.basename(inputFilepath, extname);
+            const relative = path.relative(schemasDir, dirname);
+            const outputBase = path.join(__dirname, '..', 'transformed');
 
-              standalone.importUris.forEach((uri) => {
-                const importUri = 'urn:us:gov:ic:' + uri.replace(/\.\w+$/, '.jsonld');
-                standalone.g.add(importUri, RDF_TYPE, ONTOLOGY_TYPE);
-                standalone.g.add(ontologyUri, IMPORTS_PROPERTY, importUri);
+            convienence.g.addAll(standalone.g);
+            Object.assign(convienence.namespaces, standalone.namespaces);
+            await writeGraph(convienence, path.join(outputBase, 'convenience'));
+
+            standalone.importUris.forEach((uri) => {
+              const importUri = 'urn:us:gov:ic:' + uri.replace(/\.\w+$/, '.jsonld');
+              standalone.g.add(importUri, RDF_TYPE, ONTOLOGY_TYPE);
+              standalone.g.add(ontologyUri, IMPORTS_PROPERTY, importUri);
+            });
+            await writeGraph(standalone, path.join(outputBase, 'standalone'));
+
+            async function writeGraph(p: Package, outputDir: string) {
+              outputDir = path.join(outputDir, relative);
+              const rdf = converters.rdfjson2rdfxml(p.g);
+              const json1 = await ldtr.read({ data: rdf, type: 'application/rdf+xml' });
+              const context = _.invert(p.namespaces);
+              const json2 = await jsonld.compact(json1, context);
+              const jsonText = JSON.stringify(json2, null, 2);
+              if (!fs.existsSync(outputDir)) {
+                fs.mkdirSync(outputDir, { recursive: true });
+              }
+
+              const jsonldOutputFilepath = path.join(outputDir, `${basename}.jsonld`);
+              fs.writeFileSync(jsonldOutputFilepath, jsonText);
+
+              const flattened = await jsonld.flatten(json2);
+              const nquads = (await jsonld.toRDF(flattened, { format: NQUADS_FORMAT })).replaceAll('.jsonld', '.ttl');
+              const parser = new Parser({ format: NQUADS_FORMAT });
+              const writer = new Writer({ format: TURTLE_FORMAT, prefixes: context });
+              await parser.parse(nquads, (error: any, quad: any, prefixes: any) => {
+                if (error) console.log(`PARSE ERROR: ${error}`);
+                if (quad) {
+                  writer.addQuad(quad);
+                }
               });
-              await writeGraph(standalone, path.join(outputBase, 'standalone'));
+              let turtle = '';
+              await writer.end((error: any, result: any) => {
+                if (error) console.log(`WRITE ERROR: ${error}`);
+                turtle = result;
+              });
 
-              async function writeGraph(p: Package, outputDir: string) {
-                outputDir = path.join(outputDir, relative);
-                const rdf = converters.rdfjson2rdfxml(p.g);
-                const json1 = await ldtr.read({ data: rdf, type: 'application/rdf+xml' });
-                const context = _.invert(p.namespaces);
-                const json2 = await jsonld.compact(json1, context);
-                const jsonText = JSON.stringify(json2, null, 2);
-                if (!fs.existsSync(outputDir)) {
-                  fs.mkdirSync(outputDir, { recursive: true });
-                }
-
-                const jsonldOutputFilepath = path.join(outputDir, `${basename}.jsonld`);
-                fs.writeFileSync(jsonldOutputFilepath, jsonText);
-
-                // const graph = json2['@graph'];
-                // if (graph) {
-                //   // replace all .jsonld with .ttl
-                //   graph.forEach((e: any) => {
-                //     if (e['@type'] === ONTOLOGY_TYPE) {
-                //       if (e['@id'] === ontologyUri) {
-                //         const imports = e['owl:imports'];
-                //         if (imports) {
-                //           (Array.isArray(imports) ? imports : [imports]).forEach((anImport: any) => {
-                //             anImport['@id'] = anImport['@id'].replace(/\.\w+$/, '.ttl');
-                //           });
-                //         }
-                //       } else if (e['@id'].endsWith('.jsonld')) {
-                //         e['@id'] = e['@id'].replace(/\.\w+$/, '.ttl');
-                //       }
-                //     }
-                //   });
-                // }
-                const flattened = await jsonld.flatten(json2);
-                const nquads = (await jsonld.toRDF(flattened, { format: NQUADS_FORMAT })).replaceAll('.jsonld', '.ttl');
-                const parser = new Parser({ format: NQUADS_FORMAT });
-                const writer = new Writer({ format: TURTLE_FORMAT, prefixes: context });
-                await parser.parse(nquads, (error: any, quad: any, prefixes: any) => {
-                  if (error) console.log(`PARSE ERROR: ${error}`);
-                  if (quad) {
-                    writer.addQuad(quad);
-                  }
-                });
-                let turtle = '';
-                await writer.end((error: any, result: any) => {
-                  if (error) console.log(`WRITE ERROR: ${error}`);
-                  turtle = result;
-                });
-
-                // const flattened = await jsonld.flatten(json2);
-                // const rdfQuads = await jsonld.toRDF(flattened, { format: "application/n-quads" });
-
-                // // Create an N3 writer
-                // const writer = new Writer({ format: "text/turtle" });
-
-                // // Parse and add RDF quads to the writer
-                // flattened.forEach((quad: { subject: { value: any; }; predicate: { value: any; }; object: { datatype: { value: any; }; value: any; }; }) => {
-                //   writer.addQuad(
-                //     quad.subject.value,
-                //     quad.predicate.value,
-                //     quad.object.datatype ? `"${quad.object.value}"^^${quad.object.datatype.value}` : quad.object.value
-                //   );
-                // });
-
-                // // Convert to Turtle format
-                // const turtle: string = await new Promise((resolve, reject) => {
-                //   writer.end((error: any, result: unknown) => {
-                //     if (error) reject(error);
-                //     else resolve(result as string);
-                //   });
-                // });
-
-
-                const turtleOutputFilepath = path.join(outputDir, `${basename}.ttl`);
-                fs.writeFileSync(turtleOutputFilepath, turtle);
-              }
+              const turtleOutputFilepath = path.join(outputDir, `${basename}.ttl`);
+              fs.writeFileSync(turtleOutputFilepath, turtle);
             }
           }
         }
